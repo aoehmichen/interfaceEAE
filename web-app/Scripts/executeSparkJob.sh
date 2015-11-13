6f9eb3e30cd7d54095e57cf7c6054f24fdda4754
Ubuntu <axelfrancois.oehmichen11@imperial.ac.uk>
2015-11-13 17:21:01 +0000

    script update

--- web-app/Scripts/executeSparkJob.sh
+++ web-app/Scripts/executeSparkJob.sh
@@ -2,11 +2,12 @@
 
 args=("$@")
 STR=""
+
 for i in $(seq 0 $#)
  do
   STR+=" ${args[$i]}"
  done
 
-command='/opt/mapr/spark/spark-1.4.1/bin/spark-submit --py-files CrossValidation.zip --master yarn-client  --num-executors 2 --driver-memory 1024m  --executor-memory 512m   --executor-cores 1 $STR &'
-#We trigger the Spark Job
-sudo su - ubuntu -c command
+command="/opt/mapr/spark/spark-1.4.1/bin/spark-submit --py-files /home/ubuntu/CrossValidation.zip --master yarn-client --num-executors 2 --driver-memory 1024m --executor-memory 512m --executor-cores 1 $STR"
+
+sudo -u ubuntu  $command &
