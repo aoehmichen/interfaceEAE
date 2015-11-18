5627a0653e6e848f51b1810b7e7cd318775fa910
Axel Oehmichen <axelfrancois.oehmichen11@imperial.ac.uk>
2015-11-18 14:00:02 +0000

    adjustements

--- grails-app/controllers/com/eae/SparkSubmitController.groovy
+++ grails-app/controllers/com/eae/SparkSubmitController.groovy
@@ -1,17 +1,23 @@
 package com.eae
 
+import org.codehaus.groovy.grails.web.json.JSONObject
+
 class SparkSubmitController {
 
     def sparkSubmitService
 
     def runSubmit = {
         final String scriptDir = getWebAppFolder() + '/Scripts/';
-        println(params)
-        String workflow = params.workflow;
-        String dataFileName = params.dataFileName;
-        String additionalFileName = params.additionalFileName
-        def workflowSpecificParameters = params.workflowSpecificParameters; // sparkSubmitService.prepareSpecificParameters(params)
-        def mongoDocumentID = params.mongoDocumentID;
+        println(response)
+        println(getResponse())
+
+        JSONObject myParams = new JSONObject(getResponse().toString())
+
+        String workflow = myParams.workflow;
+        String dataFileName = myParams.dataFileName;
+        String additionalFileName = myParams.additionalFileName
+        def workflowSpecificParameters = myParams.workflowSpecificParameters; // sparkSubmitService.prepareSpecificParameters(params)
+        def mongoDocumentID = myParams.mongoDocumentID;
 
         sparkSubmitService.sparkSubmit(scriptDir, workflow, dataFileName, additionalFileName, workflowSpecificParameters, mongoDocumentID)
 
