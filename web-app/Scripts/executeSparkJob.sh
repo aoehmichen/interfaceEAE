e68e50157b167cc957031d8b755cc5fb27e2d80f
axelfrancois.oehmichen11@imperial.ac.uk <axelfrancois.oehmichen11@imperial.ac.uk>
2015-11-25 17:31:08 +0000

    Fix the script for Ubuntu + CDH

--- web-app/Scripts/executeSparkJob.sh
+++ web-app/Scripts/executeSparkJob.sh
@@ -8,6 +8,6 @@ for i in $(seq 0 $#)
   STR+=" ${args[$i]}"
  done
 
-command="/opt/mapr/spark/spark-1.4.1/bin/spark-submit --py-files /home/ubuntu/CrossValidation.zip --master yarn-client --num-executors 2 --driver-memory 1024m --executor-memory 512m --executor-cores 1 $STR"
+command="/usr/bin/spark-submit --py-files /home/ubuntu/CrossValidation.zip --master yarn-client --num-executors 2 --driver-memory 1024m --executor-memory 512m --executor-cores 1 $STR"
 
 sudo -u ubuntu  $command &
