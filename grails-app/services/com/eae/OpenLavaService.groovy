bc430c2dfe1791ddb80c2df352f7df5dbd2a325c
Axel Oehmichen <axelfrancois.oehmichen11@imperial.ac.uk>
2016-07-22 17:18:18 +0100

    test

--- grails-app/services/com/eae/OpenLavaService.groovy
+++ grails-app/services/com/eae/OpenLavaService.groovy
@@ -7,8 +7,11 @@ class OpenLavaService {
 
     def openLavaBsub(){
 
-        def sout = new StringBuilder(), serr = new StringBuilder()
+        def sout = new StringBuilder()
+        def serr = new StringBuilder()
+        log.error("Here 2")
         def proc = 'bsub -m "test-spark-master" \'bash spark.sh\''.execute()
+        log.error("Here 3")
         proc.consumeProcessOutput(sout, serr)
         proc.waitForOrKill(1000)
         println "out> $sout err> $serr"
