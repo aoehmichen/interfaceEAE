155abe16076f6391d554b23c719218941dde2154
Axel Oehmichen <axelfrancois.oehmichen11@imperial.ac.uk>
2016-08-26 16:55:50 +0100

    update the bash scripts

--- web-app/Scripts/JobGPU.sh
+++ web-app/Scripts/JobGPU.sh
@@ -1,35 +1,56 @@
 #!/bin/bash
-
 args=("$@")
 OPEN_LAVA_MASTER=$(hostname | cut -d. -f1)
-HOSTS=($(<spark_cluster.txt))
+CLUSTER="gpu"
 JOB_NAME=${args[0]}
 SCRIPTS_ZIP=${args[1]}
-PYTHON_MAIN_FILE=${args[2]}
+MAIN_FILE=${args[2]}
+CONFIG_FILE=${args[3]} #"config.txt"
 i=0
 
-parallel-rsync -h destfile.txt -p 10 $SCRIPTS_ZIP  ~/
+exports="export LD_LIBRARY_PATH=/usr/local/cuda/lib64;
+         export CUDA_HOME=/usr/local/cuda;"
 
-echo "$JOB_NAME"
+#parallel-rsync -h destfile.txt -p 10 $SCRIPTS_ZIP  ~/
 
-echo "$SCRIPTS_ZIP"
 
-if [ $? -eq 0 ]
+#TODO add check and exit codes to prevent some misbehaviours
+function gpu_submit {
+  echo "scp $OPEN_LAVA_MASTER:~/$SCRIPTS_ZIP .;
+        unzip $SCRIPTS_ZIP -d $JOB_NAME;
+        mkdir $JOB_NAME/results;
+        $exports
+        $python_submit;
+        cd ~/$JOB_NAME/results/;
+        zip -r $result_zip *;
+        scp $result_zip $OPEN_LAVA_MASTER:~/results_$JOB_NAME/;
+        rm -rf ~/$JOB_NAME;"
+}
+
+
+if [ -d ~/results_$JOB_NAME ]
  then
+  echo "The folder already exists for job: $JOB_NAME"
+  exit 1;
+ else
+  mkdir  ~/results_$JOB_NAME
   while read line;
    do
-    python_submit="export LD_LIBRARY_PATH=/usr/local/cuda/lib64; export CUDA_HOME=/usr/local/cuda; python ~/$JOB_NAME/$PYTHON_MAIN_FILE  $line"
-    result_zip="results_"$JOB_NAME"_"${HOSTS[$i]}".zip"
-    echo "$JOB_NAME"
-    #bsub -m "$OPEN_LAVA_MASTER" -J "task1"${HOSTS[$i]} "rcp $SCRIPTS_ZIP ${HOSTS[$i]}:~;"
-    bsub -m "${HOSTS[$i]}" -J "$JOB_NAME"_"${HOSTS[$i]}"  "unzip $SCRIPTS_ZIP -d $JOB_NAME; mkdir $JOB_NAME/results; $python_submit; cd ~/$JOB_NAME/results/; zip -r $result_zip *; scp $result_zip $OPEN_LAVA_MASTER:~/results/; rm -rf ~/$JOB_NAME;"
-    i=$((i+1))
-    if [ $i = ${#HOSTS[@]} ]; then
-       break;
-    fi
-   done < config.txt
-  exit $$;
+    python_submit="python ~/$JOB_NAME/$MAIN_FILE  $line"
+    result_zip="results_"$JOB_NAME"_"$i".zip"
+    submit=$(gpu_submit)
+    bsub -q "$CLUSTER" -J "$JOB_NAME"_"$i" -r "$submit"
+    #job=$(bsub -q "gpu" -J "$JOB_NAME"_"$i" -r "$submit")
+    #job_id=( $(echo  "$job" | xargs -n 1 | grep "<[0-9]" | cut -b2- | sed s"/.$//" ))
 
-else
-  echo "The parallel rsync failed. Error code: $?" ;
+    #i=$((i+1))
+#    if [ -z $ALL_JOBS ]
+#    then
+#        ALL_JOBS="ended($job_id)";
+#    else
+#        ALL_JOBS=$ALL_JOBS"&&ended($job_id)";
+#    fi
+   done < $CONFIG_FILE
+   #bsub -w "$ALL_JOBS" -J "Cleanup"  "sleep 20;"
+  exit $$;
 fi
