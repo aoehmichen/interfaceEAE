4c7347677acbb781880edf3e7cd07c6657cd5563
Axel Oehmichen <axelfrancois.oehmichen11@imperial.ac.uk>
2016-10-11 14:29:35 +0100

    typo

--- grails-app/controllers/com/eae/UtilitiesController.groovy
+++ grails-app/controllers/com/eae/UtilitiesController.groovy
@@ -4,14 +4,14 @@ import javax.servlet.http.HttpServletResponse
 
 class UtilitiesController {
 
-    static allowedMethods = [isALive: ['GET']]
+    static allowedMethods = [isAlive: ['GET']]
 
     /**
      * Method that allows to test if the REST Api server is alive or not
      *
      * @return {int}: Sends back 200 to tell that the server is alive.
      */
-    def isALive = {
+    def isAlive = {
         render HttpServletResponse.SC_OK;
     }
 
