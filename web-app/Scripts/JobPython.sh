458e85f4f84d2d638565316dc5c960f830db5750
Axel Oehmichen <axelfrancois.oehmichen11@imperial.ac.uk>
2016-10-24 01:04:09 +0100

    stupid me

--- web-app/Scripts/JobPython.sh
+++ web-app/Scripts/JobPython.sh
@@ -3,17 +3,19 @@ args=("$@")
 OPEN_LAVA_MASTER=$(hostname | cut -d. -f1)
 CLUSTER=${args[0]}
 JOB_NAME=${args[1]}
-SCRIPTS_ZIP=${args[2]}
+SCRIPTS_ZIP_ON_REMOTE_HOST=${args[2]}
 MAIN_FILE=${args[3]}
 CONFIG_FILE=${args[4]} #"config.txt"
 REMOTE_HOST=${args[5]}
 i=0
 
+SCRIPTS_ZIP=$JOB_NAME".zip"
+
 #TODO add check and exit codes to prevent some misbehaviours
 function python_submit_function {
   echo "mkdir -p /tmp/$JOB_NAME;
         cd /tmp/$JOB_NAME;
-        scp $REMOTE_HOST:$SCRIPTS_ZIP .;
+        scp $REMOTE_HOST:$SCRIPTS_ZIP_ON_REMOTE_HOST .;
         unzip $SCRIPTS_ZIP -d /tmp/$JOB_NAME;
         mkdir -p /tmp/$JOB_NAME/results;
         $python_submit
