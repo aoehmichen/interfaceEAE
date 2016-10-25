a5e6f5f401ae266c0d3e190292c9d916e4bc87ca
aoehmich <axelfrancois.oehmichen11@imperial.ac.uk>
2016-10-25 16:14:32 -0700

    major fix

--- web-app/Scripts/JobGPU.sh
+++ web-app/Scripts/JobGPU.sh
@@ -3,11 +3,13 @@ args=("$@")
 OPEN_LAVA_MASTER=$(hostname | cut -d. -f1)
 CLUSTER=${args[0]}
 JOB_NAME=${args[1]}
-SCRIPTS_ZIP=${args[2]}
+SCRIPTS_ZIP_ON_REMOTE_HOST=${args[2]}
 MAIN_FILE=${args[3]}
 CONFIG_FILE=${args[4]} #"config.txt"
 REMOTE_HOST=${args[5]}
 i=0
+
+SCRIPTS_ZIP=$JOB_NAME".zip"
 
 source /etc/profile.d/openlava.sh
 
@@ -19,7 +21,7 @@ exports="export LD_LIBRARY_PATH=/usr/local/cuda/lib64;
 function gpu_submit_function {
   echo "mkdir -p /tmp/$JOB_NAME;
         cd /tmp/$JOB_NAME;
-        scp $OPEN_LAVA_MASTER:$SCRIPTS_ZIP .;
+        scp $REMOTE_HOST:$SCRIPTS_ZIP_ON_REMOTE_HOST .;
         unzip $SCRIPTS_ZIP -d /tmp/$JOB_NAME;
         mkdir /tmp/$JOB_NAME/results;
         $exports
@@ -30,20 +32,12 @@ function gpu_submit_function {
         rm -rf /tmp/$JOB_NAME;"
 }
 
-
-if [ -d ~/results_$JOB_NAME ]
- then
-  echo "The folder already exists for job: $JOB_NAME"
-  exit 1;
- else
-  mkdir  ~/results_$JOB_NAME
-  while read line;
-   do
-    python_submit="python /tmp/$JOB_NAME/$MAIN_FILE  $line"
-    result_zip="results_"$JOB_NAME"_"$i".zip"
-    submit=$(gpu_submit_function)
-    bsub -q "$CLUSTER" -J "$JOB_NAME"_"$i" -r "$submit"
-    i=$((i+1))
-   done < $CONFIG_FILE
-  exit 0;
-fi
+while read line;
+ do
+  python_submit="python /tmp/$JOB_NAME/$MAIN_FILE  $line"
+  result_zip="results_"$JOB_NAME"_"$i".zip"
+  submit=$(gpu_submit_function)
+  bsub -q "$CLUSTER" -J "$JOB_NAME"_"$i" -r "$submit"
+  i=$((i+1))
+ done < $CONFIG_FILE
+exit 0;
