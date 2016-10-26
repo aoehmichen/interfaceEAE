e206b29bcb9ee6b0bc9f7716bc78973a3952d648
aoehmich <axelfrancois.oehmichen11@imperial.ac.uk>
2016-10-26 07:50:16 -0700

    small R update

--- web-app/Scripts/JobR.sh
+++ web-app/Scripts/JobR.sh
@@ -1,38 +1,40 @@
 #!/bin/bash
 args=("$@")
 OPEN_LAVA_MASTER=$(hostname | cut -d. -f1)
-CLUSTER="gpu"
-JOB_NAME=${args[0]}
-SCRIPTS_ZIP=${args[1]}
-MAIN_FILE=${args[2]}
-CONFIG_FILE=${args[3]} #"config.txt"
+CLUSTER=${args[0]}
+JOB_NAME=${args[1]}
+SCRIPTS_ZIP_ON_REMOTE_HOST=${args[2]}
+MAIN_FILE=${args[3]}
+CONFIG_FILE=${args[4]} #"config.txt"
+REMOTE_HOST=${args[5]}
 i=0
+
+SCRIPTS_ZIP=$JOB_NAME".zip"
+
+source /etc/profile.d/openlava.sh
 
 #TODO add check and exit codes to prevent some misbehaviours
 function R_submit {
-  echo "scp $OPEN_LAVA_MASTER:~/$SCRIPTS_ZIP .;
-        unzip $SCRIPTS_ZIP -d $JOB_NAME;
-        mkdir $JOB_NAME/results;
+  echo "mkdir -p /tmp/$JOB_NAME;
+        cd /tmp/$JOB_NAME;
+        scp $REMOTE_HOST:$SCRIPTS_ZIP_ON_REMOTE_HOST .;
+        unzip $SCRIPTS_ZIP -d /tmp/$JOB_NAME;
+        mkdir /tmp/$JOB_NAME/results;
         $R_submit;
-        cd ~/$JOB_NAME/results/;
+        cd /tmp/$JOB_NAME/results/;
         zip -r $result_zip *;
-        scp $result_zip $OPEN_LAVA_MASTER:~/results_$JOB_NAME/;
-        rm -rf ~/$JOB_NAME;"
+        ssh $REMOTE_HOST 'mkdir -p /home/ubuntu/jupyter/eae_results_$JOB_NAME/';
+        scp $result_zip $REMOTE_HOST:/home/ubuntu/jupyter/eae_results_$JOB_NAME/;
+        rm -rf /tmp/$JOB_NAME;"
 }
 
 
-if [ -d ~/results_$JOB_NAME ]
- then
-  echo "The folder already exists for job: $JOB_NAME"
-  exit 1;
- else
-  mkdir  ~/results_$JOB_NAME
-  while read line;
-   do
-    R_submit="R ~/$JOB_NAME/$MAIN_FILE  $line"
-    result_zip="results_"$JOB_NAME"_"$i".zip"
-    submit=$(gpu_submit)
-    bsub -q "$CLUSTER" -J "$JOB_NAME"_"$i" -r "$submit"
-   done < $CONFIG_FILE
-  exit 0;
-fi
+while read line;
+  do
+   R_submit="R ~/$JOB_NAME/$MAIN_FILE  $line"
+   result_zip="results_"$JOB_NAME"_"$i".zip"
+   submit=$(R_submit)
+   bsub -q "$CLUSTER" -J "$JOB_NAME"_"$i" -r "$submit"
+   i=$((i+1))
+  done < $CONFIG_FILE
+exit 0;
