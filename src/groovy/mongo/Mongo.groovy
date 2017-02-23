2c9c2191f5c8f5dbf32c60e6d719574aa17b6cf7
Axel Oehmichen <axelfrancois.oehmichen11@imperial.ac.uk>
2017-02-23 17:25:37 +0000

    checked out wrong base branch ....

--- src/groovy/mongo/Mongo.groovy
+++ src/groovy/mongo/Mongo.groovy
@@ -16,10 +16,9 @@ public class MongoFactory {
      * @return {MonClient} : client to be used for running the queries
      */
     static def getMongoConnection(String IPAddress, String port, String user, String database, char[] password){
-        int portToUse = Integer.parseInt(port)
-        ServerAddress address = new ServerAddress(IPAddress, portToUse)
-        MongoCredential credential = MongoCredential.createCredential(user, database, password);
-        return new MongoClient(address, Arrays.asList(credential));
+        int portToUse = Integer.parseInt(port);
+        ServerAddress address = new ServerAddress(IPAddress, portToUse);
+        return new MongoClient(address);
     }
 
 }
